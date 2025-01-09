c48c66188a0fa3a85b3a6710281de2e7fec52e21
henryhoangduong <henryhoangduong@gmail.com>
2025-01-09 11:20:33 +0700

    mapping user and userType entity

--- src/main/java/com/henry/jobportal/JobportalApplication.java
+++ src/main/java/com/henry/jobportal/JobportalApplication.java
@@ -2,8 +2,11 @@ package com.henry.jobportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.henry.jobportal.entity")
 public class JobportalApplication {
 
 	public static void main(String[] args) {
