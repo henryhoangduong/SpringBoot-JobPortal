6414c098b13ac598f0c90f0ecf00b0637f00dcdf
henryhoangduong <henryhoangduong@gmail.com>
2025-01-10 08:09:11 +0700

    add dashboard.html

--- src/main/java/com/henry/jobportal/controller/UsersController.java
+++ src/main/java/com/henry/jobportal/controller/UsersController.java
@@ -3,10 +3,12 @@ package com.henry.jobportal.controller;
 import com.henry.jobportal.entity.Users;
 import com.henry.jobportal.entity.UsersType;
 import com.henry.jobportal.services.UsersTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,5 +27,11 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users) {
+        System.out.println("Users: "+users);
+        return "dashboard";
     }
 }
