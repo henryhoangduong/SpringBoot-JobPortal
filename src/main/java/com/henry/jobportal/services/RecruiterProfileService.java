1a999b0860930bcb778c9dad580bf0a2c36d9433
henryhoangduong <henryhoangduong@gmail.com>
2025-01-19 10:46:14 +0700

    create recruiter profile - controller and service

---
+++ src/main/java/com/henry/jobportal/services/RecruiterProfileService.java
@@ -0,0 +1,20 @@
+package com.henry.jobportal.services;
+
+import com.henry.jobportal.entity.RecruiterProfile;
+import com.henry.jobportal.repository.RecruiterProfileRepository;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@Service
+public class RecruiterProfileService {
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+    public Optional<RecruiterProfile> getOne(Integer id){
+        return recruiterProfileRepository.findById(id);
+    }
+}
